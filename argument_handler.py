9bf26290e555b0ccfd5bb420bfec89cce4719945
Munkácsi Zoltán <munkacsizoltan05@gmail.com>
2023-04-05 23:11:01 +0200

    added examples to help message

--- argument_handler.py
+++ argument_handler.py
@@ -8,11 +8,11 @@ class InputArgumentReader:
 
     def _get_input_arguments(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--features','-f', nargs='*',help='Features for model training, separated by whitespace. For example: citric_acid residual_sugar chlorides')
-        parser.add_argument('--model_arguments','-a', nargs='*',help='Arguments of the model, separated by whitespace. For example: kernel rbf degree 3')
+        parser.add_argument('--features','-f', nargs='*',help='Features for model training, separated by whitespace. E.g. citric_acid residual_sugar chlorides')
+        parser.add_argument('--model_arguments','-a', nargs='*',help='Arguments of the model, separated by whitespace. E.g. kernel rbf degree 3')
 
-        parser.add_argument('--datasource_location','-d', type=str, help='Relative path of the datasource')
-        parser.add_argument('--result_location','-r', type=str, help='Relative path where the result will be saved')
+        parser.add_argument('--datasource_location','-d', type=str, help='Relative path of the datasource. E.g. ..\data\ ')
+        parser.add_argument('--result_location','-r', type=str, help='Relative path where the result will be saved. E.g. result\ ')
         parser.add_argument('--test_data_proportion','-t', type=float, help='It should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split')
 
         arguments = {}
